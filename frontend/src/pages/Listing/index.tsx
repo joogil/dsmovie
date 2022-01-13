9b9647e2d15d42ffeed79dac8dd253cc636549eb
João Gilberto <jgsopro@gmail.com>
2022-01-13 08:19:56 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Pagination from "components/Pagination";
 
 function Listing() {
     return (
-        <h1>Página de Listagem</h1>
+        <Pagination />
     );
 }
 
