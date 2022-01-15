6ce8e6cddfad1413570c665828b9702fc8e972aa
João Gilberto <jgsopro@gmail.com>
2022-01-15 01:48:10 -0300

    Adicionando arquivo de configuração para o netlify

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,7 +8,7 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/joogil" target="_blank" rel="noreferrer">
+                    <a href="https://github.com/joogil" target="_blank" rel="noopener noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="dsmovie-contact-link">/devsuperior</p>
